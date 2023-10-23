5a2bfc6ee3b077884733d236d75bfc8517e1cab8
mmcgillycuddy <maevemcgillycuddy@gmail.com>
2023-10-23 17:14:00 +1100

    update manylm keepBoot example

--- misc/manylm_keepBoot.R
+++ misc/manylm_keepBoot.R
@@ -17,7 +17,7 @@ str(an)
 
 # p = (#exceeding observed stat + 1)/(#nboot+1)
 
-(sum(an$bootStat > an$table[2,3] ) + 1 )/(50 + 1)
+(sum(an$bootStat > an$table[2,3] ) + 2 )/(50 + 1)
   
 
 
@@ -27,7 +27,28 @@ Y = with(iris, cbind(Sepal.Length,Sepal.Width,Petal.Length,Petal.Width))
 iris_mlm=manylm(Y~Species,data=iris)
 iris_an <- anova(iris_mlm, nBoot = 100, keep.boot = T)
 
+iris_an$bootStat
 # p-value given 
 iris_an$table[2,4]  == 2/ (100 + 1)
 
-(sum(iris_an$bootStat > log(iris_an$table[2,3]) ) + 1 )/(50 + 1)
+(sum(iris_an$bootStat > iris_an$table[2,3] ) + 1 )/(100 + 1)
+
+
+
+data(spider)
+spiddat <- log(spider$abund+1)
+spiddat <- mvabund(spiddat)
+spidx <- as.matrix(spider$x)
+fit <- manylm( spiddat ~ (spidx) ) 
+fit0 <- manylm( spiddat ~ (spidx[,1:5]) )
+spidan <-anova(fit, fit0)
+
+pan_spid <-pboot <- rep(NA, length(nboots))
+for(i in 1:length(nboots)){
+  spidan <-anova(fit, fit0, nBoot = nboots[i], keep.boot = T)
+  pan_spid[i] <- spidan$table[[4]][2]
+  pboot[i] <- (sum(spidan$bootStat > spidan$table[2,3] ) + 2 )/(nboots[i] + 1) #bug in code (should be + 1 instead of +2)
+  
+}
+pboot == pan_spid 
+
